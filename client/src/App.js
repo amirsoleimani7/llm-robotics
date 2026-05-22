706a5ea5fc987f59ca978e43adac65721a448c7c
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-05-22 17:11:04 +0330

    fix : switching between voice nad text fixed

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import InputArea from "./components/MainArea/InputArea";
 
 function App() {
   return (
-    <div className="bg-main-color text-white w-full h-screen flex justify-center">
+    <div className="bg-main-color text-white w-full h-screen flex justify-center p-2">
       <InputArea>
       </InputArea>
     </div>
--- client/src/components/MainArea/InputArea.jsx
+++ client/src/components/MainArea/InputArea.jsx
@@ -1,17 +1,40 @@
+import { useState } from "react";
 import { IoSend } from "react-icons/io5";
-
+import { LuBrain } from "react-icons/lu";
+import { MdOutlineKeyboardVoice } from "react-icons/md";
 
 function InputArea() {
+  const [is_voice, SetIsVoice] = useState(true);
+  const handle_user_input = (e) => {
+    e.target.value !== "" ? SetIsVoice(false) : SetIsVoice(true);
+  };
+
   return (
     <>
-      <div className="flex flex-col bg-second-color w-[700px] h-36 pt-4 p-4 pb-6 rounded-[2rem] mt-auto mb-8 duration-300 ease-in-out transition-all scrollbar-thin">
-        <textarea type="text" placeholder="Message to Robot" className="bg-transparent focus:outline-none w-full h-[80%] resize-none scrollbar-thin"/>
-        <div className="flex mt-auto">
-            <button className="ml-auto rounded-full bg-gray-500 p-2 flex justify-center items-center">
-                <IoSend>
-                    
-                </IoSend>
-            </button>
+      <div className="flex flex-col bg-second-color w-[700px] h-36 p-6 rounded-[2rem] mt-auto mb-8 duration-300 ease-in-out transition-all border border-gray-500">
+        <textarea
+          type="text"
+          placeholder="Message to Robot"
+          onChange={handle_user_input}
+          className="bg-transparent focus:outline-none w-full h-[80%] resize-none text-white font-semibold"
+        />
+        <div className="flex justify-between mt-auto w-full">
+          <button className="flex items-center justify-center gap-1 border px-2 border-gray-500 rounded-[2rem] text-sm ">
+            <LuBrain></LuBrain>
+            <p>Model</p>
+          </button>
+          <button
+            className="ml-auto rounded-full p-2 flex justify-center items-center border border-gray-500 duration-300 ease-in-out"
+            style={{
+              background: `${!is_voice ? "gray" : ""}`,
+            }}
+          >
+            {is_voice ? (
+              <MdOutlineKeyboardVoice></MdOutlineKeyboardVoice>
+            ) : (
+              <IoSend></IoSend>
+            )}
+          </button>
         </div>
       </div>
     </>
