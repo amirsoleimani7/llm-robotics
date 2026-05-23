c1150c4cb3c62445ef248d1f2d6b97272eaa26e5
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-05-23 20:06:37 +0330

    add : kidna fixed the overlapping and flex-none and shrink problem

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,16 @@ import Side from "./components/SideBar/Side";
 
 function App() {
   return (
-    <div className="bg-main-color text-white w-full h-screen flex">
-      <Side/>
-      <div className="flex justify-center w-full p-4 transition-all ease-in-out duration-300">
-        {/* chats shoudl come here is as well */}
-        <InputArea/>
-        </div>
+    <div className="bg-main-color text-white w-full h-screen flex justify-between">
+      <Side />
+      <div className="w-[800px] px-2 shrink flex justify-center mx-auto tranistion-all ease-in-out duration-300 mb-4">
+        <InputArea />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/*
+ */
--- client/src/components/SideBar/Side.jsx
+++ client/src/components/SideBar/Side.jsx
@@ -3,6 +3,52 @@ import { topButtons, bottomButtons } from "./buttons";
 import { IoIosShareAlt } from "react-icons/io";
 import { RxHamburgerMenu } from "react-icons/rx";
 
+/*
+
+        <button
+          className="absolute top-4 -translate-x-full max-md:translate-x-full max-md:left-2 duration-300 ease-in-out transition-all "
+          onClick={handle_sidebar}
+        >
+          <RxHamburgerMenu />
+        </button>
+      <div
+        className="duration-300 ease-in-out h-full border-r border-gray-700  max-lg:fixed max-lg:left-0 bg-red-300"
+        // style={{
+        //   width: `${is_open ? "250px" : "0px"}`,
+        //   opacity: `${is_open ? "100" : "100"}`,
+        // }}
+      >
+        <div className="w-[100vw] absolute  top-2 left-2 gap-1 duration-300 ease-in-out tarnsform-all">
+          <div className="flex gap-2">
+            <img
+              src=""
+              alt=""
+              className="bg-red-100 rounded-[2rem] aspect-square"
+            />
+            <div className="flex bg-main-color-2 gap-2 p-1 rounded-[2rem] border border-gray-700">
+              {topButtons.map((btn) => {
+                const Icon = btn.icon;
+                return (
+                  <button
+                    key={btn.id}
+                    className="w-full aspect-square p-1 rounded-[2rem] flex justify-center items-center hover:bg-main-color-3 duration-200 ease-in-out active:*:scale-[1.10]"
+                    onClick={handlers[btn.onClick]}
+                  >
+                    <Icon className="scale-105" />
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          <button className="rounded-[2rem] p-3 b ml-auto">
+            <IoIosShareAlt />
+          </button>
+        </div>
+        
+      </div>
+
+        */
+
 function Side() {
   const [is_open, setIs_open] = useState(false);
 
@@ -21,61 +67,25 @@ function Side() {
   return (
     <>
       <div
-        className=" duration-300 ease-in-out h-full border-r border-gray-700  max-lg:fixed max-lg:left-0 bg-red-300 max-lg:"
+        className="duration-300 ease-in-out h-full border-r border-gray-700 max-lg:fixed max-lg:left-0 bg-red-300 flex-none"
         style={{
           width: `${is_open ? "250px" : "0px"}`,
           opacity: `${is_open ? "100" : "100"}`,
         }}
       >
-        {/* <div className="w-[50px] h-full flex flex-col py-2 items-center justify-between"> */}
-
-        <button className="absolute top-4 -translate-x-full max-md:translate-x-full max-md:left-2 duration-300 ease-in-out transition-all "
-        onClick={handle_sidebar} >
-            <RxHamburgerMenu/>
-        </button>
-        
-        <button className="absolute right-2 top-2 rounded-[2rem] p-3">
-          <IoIosShareAlt />
-        </button>
-
-        <div className="w-[180px] absolute flex top-2 left-2 gap-1 duration-300 ease-in-out tarnsform-all max-md:-translate-x-full max-md:left-0">
-          <img
-            src=""
-            alt=""
-            className="bg-red-100 w-1/4 rounded-[2rem] aspect-square"
-          />
-          <div className="flex bg-main-color-2 w-3/4 gap-2 p-1 rounded-[2rem] border border-gray-700">
-            {topButtons.map((btn) => {
-              const Icon = btn.icon;
-              return (
-                <button
-                  key={btn.id}
-                  className="w-full aspect-square p-1 rounded-[2rem] flex justify-center items-center hover:bg-main-color-3 duration-200 ease-in-out active:*:scale-[1.10]"
-                  onClick={handlers[btn.onClick]}
-                >
-                  <Icon className="scale-105" />
-                </button>
-              );
-            })}
-          </div>
-          {/* </div> */}
-
-          {/* <div>
-                        
-          </div> */}
-          {/* <div className="flex flex-col gap-2 w-full p-2">
-            {bottomButtons.map((btn, index) => {
-              const Icon = btn.icon;
-              return (
-                <button
-                  key={btn.id}
-                  className="w-full aspect-square p-1 rounded-lg flex justify-center items-center hover:bg-gray-300 hover:text-black duration-300 ease-in-out active:*:scale-[1.10]"
-                >
-                  <Icon className="scale-105" />
-                </button>
-              );
-            })}
-          </div> */}
+        <div className="flex bg-main-color-2 gap-2 p-1 rounded-[2rem] border border-gray-700">
+          {topButtons.map((btn) => {
+            const Icon = btn.icon;
+            return (
+              <button
+                key={btn.id}
+                className="w-full aspect-square p-1 rounded-[2rem] flex justify-center items-center hover:bg-main-color-3 duration-200 ease-in-out active:*:scale-[1.10]"
+                onClick={handlers[btn.onClick]}
+              >
+                <Icon className="scale-105" />
+              </button>
+            );
+          })}
         </div>
       </div>
     </>
