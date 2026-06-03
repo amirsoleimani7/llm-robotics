1956f96fa8cf3209cf70a0ee329c13c22518d69a
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-03 12:54:05 +0330

    add : fix deleting bug and making new chat bug

--- core/core/views.py
+++ core/core/views.py
@@ -22,6 +22,7 @@ def get_converastions(request):
 @api_view(['GET'])
 def get_conversation_chats(request, pk):
     if request.method == 'GET':
+        print(f"primary key is : {pk}")
         try:
             query_conv = Conversation.objects.get(conversation_id=pk)
             messages = Message.objects.filter(conversation=query_conv)
